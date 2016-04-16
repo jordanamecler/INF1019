f49d2128e1e4aded61128b7ae7e888637103143f
leotok <leo.wajnsztok@gmail.com>
2016-04-16 01:27:36 -0300

    leitura entrada, execv basico

--- main.c
+++ main.c
@@ -5,10 +5,46 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+int main() {
+
+	FILE *entrada;
+	char comando[5], path[15], prioridade_string[15];
+	int prioridade = 0;
+	pid_t pid1;
+
+	entrada = fopen("entrada.txt","r");
+	if(entrada == NULL) {
+		printf("Erro ao abrir entrada.txt\n");
+		exit(1);
+	}
+
+	while(fscanf(entrada, "%s", comando) == 1) {
+		fscanf(entrada, "%s %s", path, prioridade_string);
+
+		printf("%s %s %s\n", comando, path, prioridade_string);
 
 
-int main(int argc, char * argv[]) {
+		prioridade = prioridade_string[11] - '0';
 
+		if((pid1 = fork()) < 0) {
+			printf("Erro ao criar processo filho.\n");
+			exit(1);
+		}
+		else if(pid1 == 0) {
+			if(execv(path, NULL) < 0) {
+				printf("Erro ao executar o programa %s.\n", path);
+			}
+		}
+		else if(pid1 > 0) {
+			int status;
+
+			wait(&status);
+			printf("De volta ao pai.\n");
+		}
+
+	}
+
+	fclose(entrada);
 
 	return 0;
 }
---
+++ teste1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main()
+{
+	printf("teste1\n");
+
+	return 0;
+}

f8fb6b0ff60efab805864d6006a0a6538a2a69b0
leotok <leo.wajnsztok@gmail.com>
2016-04-16 00:36:19 -0300

    commit inicial

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+
+
+int main(int argc, char * argv[]) {
+
+
+	return 0;
+}

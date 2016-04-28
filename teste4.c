94edd94b10c4d2e005fe1b1766c0cfc053b72ba8
leotok <leo.wajnsztok@gmail.com>
2016-04-27 23:55:31 -0300

    teste grandes

---
+++ teste4.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+// compilar esse programa antes de rodar main.c
+
+int main()
+{
+	// CPU-BOUND
+	int i, j;
+	for( i = 0; i < 5000000; i++) {
+		// roda por um tempo e printa 10x 
+		for (j = 0; j < 5000; j++) {
+			if ((j % 1000 == 0) && (i % 1000000 == 0)) {
+				printf("Teste 4 rodando.\n");	
+			}
+		}
+	}
+
+	return 0;
+}

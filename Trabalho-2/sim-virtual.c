433b6ff60f08f1c7a8e85074d98e4a3e28ee8112
leotok <leo.wajnsztok@gmail.com>
2016-06-12 21:00:12 -0300

    updates

--- Trabalho-2/sim-virtual.c
+++ Trabalho-2/sim-virtual.c
@@ -2,15 +2,67 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
+
+typedef struct tabelaPagina {
+
+	int r;
+	int m;
+	int ultimoAcesso;	
+} TabelaPagina;
 
 unsigned pegaIndicePagina(unsigned addr, int paginaTam) {
 
-	int p = 13 ;
+	return addr >> log2(paginaTam << 10);;
+}
 
-	if( paginaTam == 16 ) p = 14;
-	else if (paginaTam == 32 ) p = 15;
+unsigned pegaTamanhoTabelaPaginas(int paginaTam) {
 
-	return addr >> p;
+	return pow(2, 32 - log2(paginaTam << 10));
+}
+
+int * criaVetorPagina(int vetorPaginasTam) {
+
+	int i;
+	int *vetorPaginas = (int *)malloc(sizeof(int) * vetorPaginasTam);
+
+	if(!vetorPaginas) {
+		printf("Erro na alocacao do vetor de paginas.\n");
+		exit(1);
+	}
+	for(i = 0; i < vetorPaginasTam; i++) {
+		vetorPaginas[i] = -1;
+	}
+
+	return vetorPaginas;
+}
+
+TabelaPagina * criaVetorTabelaPaginas(int paginaTam) {
+
+	int tabelaPaginaTam = pegaTamanhoTabelaPaginas(paginaTam);
+	TabelaPagina *vetorTabelaPaginas;
+
+	printf("Tamanha tabela de paginas: %d\n", tabelaPaginaTam);
+
+	vetorTabelaPaginas = (TabelaPagina *)malloc(sizeof(TabelaPagina) * tabelaPaginaTam);
+	if(!vetorTabelaPaginas) {
+		printf("Erro na alocacao do vetor de tabela de paginas\n");
+		exit(1);
+	}
+
+	memset(vetorTabelaPaginas, 0, tabelaPaginaTam * sizeof(TabelaPagina));
+	return vetorTabelaPaginas;
+
+}
+
+int buscaPaginaNaMemoria(int *vetorPaginas, int vetorPaginasTam, int indicePagina) {
+	int i;
+	for(i = 0; i < vetorPaginasTam; i++) {
+		if(vetorPaginas[i] == indicePagina) {
+			return 1;
+		}
+	}
+	return -1;
 }
 
 
@@ -21,10 +73,19 @@ int main(int argc, char *argv[])
 	// tamanho total de memoria fisica
 	// hipoteticamente disponivel entre: 128 kB e 16 MB
 
-	FILE * arq;
+	FILE *arq;
 	unsigned addr, page;
 	char algoritmo[5], path[20];
-	int paginaTam, memoriaFisicaTam, int rw;
+	int paginaTam, memoriaFisicaTam, rw, time = 0;
+	int faltasDePagina = 0;
+	int paginasEscritas = 0;
+
+	int *vetorPaginas;
+	int vetorPaginasTam;
+
+	TabelaPagina * vetorTabelaPaginas;
+
+	// Trata input
 
 	strcpy(algoritmo, argv[1]);
 	strcpy(path, argv[2]);
@@ -32,7 +93,36 @@ int main(int argc, char *argv[])
 	paginaTam =  atoi(argv[3]);
 	memoriaFisicaTam = atoi(argv[4]) ;
 
-	// printf("%s %s %d %d\n", algoritmo, path, paginaTam, memoriaFisicaTam);
+	printf("Executando o simulador...\n");
+
+	printf("Arquivo de entrada: %s\n", path);
+	printf("Tamanho da memoria fisica: %d\n", memoriaFisicaTam);
+	printf("Tamanho das paginas: %d\n", paginaTam);
+	printf("Alg de substituicao: %s\n", algoritmo);
+
+	// Testa input
+
+	if(paginaTam < 8 || paginaTam > 32) {
+		printf("Tamanho de pagina invalido. Deve ser entre 8 e 32KB.\n");
+		exit(1);
+	}
+	if(memoriaFisicaTam < 128 || memoriaFisicaTam > (16 * 1024)) {
+		printf("Tamanho de memoria fisica invalido. Deve ser entre 128KB e 16MB.\n");
+		exit(1);
+	}
+
+	// Cria vetor de paginas da memoria
+
+	vetorPaginasTam = memoriaFisicaTam / paginaTam;
+	printf("Tamanho vetor de paginas: %d\n",vetorPaginasTam );
+
+	vetorPaginas = criaVetorPagina(vetorPaginasTam);
+
+	// Cria vetor de tabela de paginas
+
+	vetorTabelaPaginas = criaVetorTabelaPaginas(paginaTam);
+
+	// Le o log
 
 	arq = fopen(path, "r");
 	if( arq == NULL ) {
@@ -42,7 +132,23 @@ int main(int argc, char *argv[])
 
 	while( fscanf(arq, "%x %c", &addr, &rw) == 2 ) {
 
+		int indicePagina = pegaIndicePagina(addr, paginaTam);
+		int paginaEstaNaMemoria = buscaPaginaNaMemoria(vetorPaginas, vetorPaginasTam, indicePagina);
+
+		// Pagina nao esta na memoria
+		if(!paginaEstaNaMemoria) {
+
+		}
+
+
+		time++;
 	}
+
+	// Libera memoria alocada
+
+	free(vetorTabelaPaginas);
+	free(vetorPaginas);
+	fclose(arq);
 
 	return 0;
 }
